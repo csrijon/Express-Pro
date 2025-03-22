dac6096e47d127c79594d8c1fd6822c6e25eaeaf
csrijon <csrijon92@gmail.com>
2025-03-22 23:51:23 +0530

    new change

---
+++ Al Whatsapp Bot/bot.js
@@ -0,0 +1,22 @@
+import express from "express";
+import pkg from "whatsapp-web.js";
+const { Client, LocalAuth } = pkg;
+import qrcode from "qrcode";
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    // proxyAuthentication: { username: 'username', password: 'password' },
+    puppeteer: { 
+        // args: ['--proxy-server=proxy-server-that-requires-authentication.example.com'],
+        headless: false,
+    }
+});
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
