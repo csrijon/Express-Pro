178ed5dbd30c86ca864e578747600eed7ca98af4
csrijon <csrijon92@gmail.com>
2025-04-21 05:55:13 +0530

    anthor change

--- Habit Tracker Api/index.js
+++ Habit Tracker Api/index.js
@@ -3,9 +3,15 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/",(req,res) => {
-  res.send("Hello World!");
+  res.render("index");
 });
+app.get("/habit",(req,res) => {
+  res.render("habit");
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
