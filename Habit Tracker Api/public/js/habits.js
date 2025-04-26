12f9fa219847296f32a627a782544ac6ac8d7603
csrijon <csrijon92@gmail.com>
2025-04-26 16:23:06 +0530

    new add

--- Habit Tracker Api/public/js/habits.js
+++ Habit Tracker Api/public/js/habits.js
@@ -1,6 +1,26 @@
 let daysclick = document.querySelectorAll(".day");
-
-daysclick.addEventListener("click", () => {
-    daysclick.style.backgroundColor = "#ef5050";
-    daysclick.style.color = "#fff";
+let reminderclick = document.querySelectorAll(".reminder");
+let selectday = "";
+daysclick.forEach(day => {
+    day.addEventListener("click", () => {
+        day.classList.toggle("active");
+        selectday = day.textContent;
+        console.log(selectday);
+    })
 });
+let selectedreminder = "";
+reminderclick.forEach(reminder => {
+    reminder.addEventListener("click", () => {
+        reminder.classList.toggle("active");
+        selectedreminder = reminder.textContent;
+        console.log(selectedreminder);
+    })
+});
+
+let addbtn = document.querySelector(".add-btn"); 
+addbtn.addEventListener("click",() => {
+    let habitname = document.querySelector("#habit-name").value;
+    let periodday = selectday;
+    let periodreminder = selectedreminder;
+}
+)
